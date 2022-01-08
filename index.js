ea9d01d0baaba319f8d6b2cf4e5274cd725eaede
Sarthakjain211001 <56023046+Sarthakjain211001@users.noreply.github.com>
2022-01-08 20:16:05 +0530

    video-4

--- index.js
+++ index.js
@@ -14,5 +14,10 @@ app.use(express.static('public'));   //This will serve index.html file from the 
 var io = socket(server)  //server because we want socket.io to work on this server which we created above.
 
 io.on('connection', function(socket){      //listening for an event
-    console.log('made socket connection', socket.id)
+    console.log('made socket connection', socket.id) 
+
+    socket.on('chat', function(data){   //When a message is sent form a client to the server. When the message comes from a client then the server will take the message
+        io.sockets.emit('chat', data);  //and will emit that message to all the sockets(i.e all the clients) connected to the server.
+    })
 })               
+
--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,21 @@
 //Make connection 
 const socket = io.connect("http://localhost:5000");
+
+//Query DOM 
+var message  = document.getElementById('message')
+var handle =  document.getElementById('handle')
+var btn = document.getElementById('send')
+var output = document.getElementById('output')
+
+//Emit events 
+btn.addEventListener('click', function(){  //listening to the send button 
+    socket.emit('chat', {                  //grab the socket between this client and the server and emit the message and send it to the server
+        message: message.value, 
+        handle: handle.value
+    });
+});
+
+//Listen for events 
+socket.on('chat', function(data){          // Listening to chat message coming from the server
+    output.innerHTML += '<p><strong>'+ data.handle +':</strong>'+ data.message + '</p>';  //showing message on the output div
+})
