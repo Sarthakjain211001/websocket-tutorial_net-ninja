1e1a451ddf23765f674bcaa4612b5a1e8c639dce
Sarthakjain211001 <56023046+Sarthakjain211001@users.noreply.github.com>
2022-01-08 21:32:27 +0530

    video-5

--- index.js
+++ index.js
@@ -13,11 +13,16 @@ app.use(express.static('public'));   //This will serve index.html file from the 
 //Socket setup 
 var io = socket(server)  //server because we want socket.io to work on this server which we created above.
 
+//Listen to events (i.e handle the messages coming from the frontend)
 io.on('connection', function(socket){      //listening for an event
-    console.log('made socket connection', socket.id) 
+    console.log('made socket connection', socket.id) ;
 
-    socket.on('chat', function(data){   //When a message is sent form a client to the server. When the message comes from a client then the server will take the message
+    socket.on('chat', function(data){   //When a message is sent form a client to the server. When the message with the name 'chat' comes from a client then the server will take the message
         io.sockets.emit('chat', data);  //and will emit that message to all the sockets(i.e all the clients) connected to the server.
+    });
+
+    socket.on('typing', function(data){    //When a message with the name 'typing' comes to the server 
+        socket.broadcast.emit('typing', data)   //Broadcast the data(ie. handle.value) to all the clients except the one feom whom it came(i.e the one who is typing)
     })
 })               
 
--- public/chat.js
+++ public/chat.js
@@ -6,16 +6,27 @@ var message  = document.getElementById('message')
 var handle =  document.getElementById('handle')
 var btn = document.getElementById('send')
 var output = document.getElementById('output')
+var isTyping = document.getElementById('isTyping')
 
-//Emit events 
+//Emit events (i.e send message to the server)
 btn.addEventListener('click', function(){  //listening to the send button 
     socket.emit('chat', {                  //grab the socket between this client and the server and emit the message and send it to the server
-        message: message.value, 
+                                          //name of the message will be chat and the data will be message.value, handle.value
+        message: message.value,             
         handle: handle.value
     });
 });
 
-//Listen for events 
-socket.on('chat', function(data){          // Listening to chat message coming from the server
+message.addEventListener('keypress', function(){
+    socket.emit('typing', handle.value)    //name of the message : typing, and data is handle.value i.e name of the client 
+})
+
+//Listen for events (i.e handle the message coming from the server)
+socket.on('chat', function(data){          // Listening to chat message coming from the server i.e when a message with the name 'chat' comes from the server
+    isTyping.innerHTML = ""
     output.innerHTML += '<p><strong>'+ data.handle +':</strong>'+ data.message + '</p>';  //showing message on the output div
+})
+
+socket.on('typing', function(data){               //Listen to the typing event i.e when a client is typing then a tping message will come from the server. So this code will listen to that message.
+    isTyping.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>'
 })
